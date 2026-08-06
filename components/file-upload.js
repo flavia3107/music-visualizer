d6c06962b15156df291caaf5a8008262bde27a08
Flavia Kosti <flaviakosti@gmail.com>
2026-08-06 09:07:42 -0400

    Code clean up - P7

--- components/file-upload.js
+++ components/file-upload.js
@@ -2,8 +2,8 @@ const audioElement = new Audio();
 audioElement.crossOrigin = 'anonymous';
 
 const fileInput = document.getElementById('audio-file-input');
-const triggerIcon = document.getElementById('btn-select-file'); // Folder icon trigger
-const fileNameInput = document.getElementById('file-name-input'); // Title display input
+const triggerIcon = document.getElementById('btn-select-file');
+const fileNameInput = document.getElementById('file-name-input');
 
 let currentObjectUrl = null;
 
@@ -14,11 +14,7 @@ function cleanupPreviousUrl() {
 	}
 }
 
-triggerIcon.addEventListener('click', () => {
-	fileInput.click();
-});
-
-fileInput.addEventListener('change', (e) => {
+function _handleUpload(e) {
 	const file = e.target.files[0];
 	if (!file) return;
 
@@ -27,5 +23,9 @@ fileInput.addEventListener('change', (e) => {
 	currentObjectUrl = URL.createObjectURL(file);
 	audioElement.src = currentObjectUrl;
 	fileNameInput.textContent = file.name;
+}
 
-});
+export function initButtons() {
+	triggerIcon.addEventListener('click', () => fileInput.click());
+	fileInput.addEventListener('change', (e) => _handleUpload(e));
+}
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import { draw, adjustCanvasSize } from './components/visualizer.js';
-
+import { initButtons } from './components/file-upload.js';
 
 window.addEventListener('resize', adjustCanvasSize);
 adjustCanvasSize();
 
+initButtons();
 draw();
